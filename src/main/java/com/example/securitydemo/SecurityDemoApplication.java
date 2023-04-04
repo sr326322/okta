3a0989b2c5b27f202bac4bb11a24faab8d8c5224
srikanthyarramati <yarramati.srikanth@gmail.com>
2023-04-04 12:36:38 +0530

    adding okta

--- src/main/java/com/example/securitydemo/SecurityDemoApplication.java
+++ src/main/java/com/example/securitydemo/SecurityDemoApplication.java
@@ -11,4 +11,10 @@ public class SecurityDemoApplication {
 		SpringApplication.run(SecurityDemoApplication.class, args);
 	}
 
+//	okta.oauth2.client-id=0oa80ud8jjwOaSvrN5d7
+//	okta.oauth2.client-secret=f9LIEd7wAkzd-oOAFXxb3N5xoUGKGuFI6d23wsGC
+//	okta.oauth2.issuer=https://dev-34734950.okta.com/oauth2/default
+//	okta.oauth2.redirect-uri=/authorization-code/callback
+//	server.port=8084
+
 }
